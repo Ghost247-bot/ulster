321a043b7aeec7d2d973dc8551d3ff122ba025b2
IDD-NAJ <Najeebiddrisu79@gmail.com>
2025-05-18 17:28:26 -0400

    Fix Supabase environment variables handling

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/ulster/',
+  define: {
+    'process.env.VITE_SUPABASE_URL': JSON.stringify('https://ckiuvcmzwvvbpmsmpamb.supabase.co'),
+    'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNraXV2Y216d3Z2YnBtc21wYW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMDU5NTEsImV4cCI6MjA2Mjg4MTk1MX0.0wVLPDpb1z_CF0qBVxUn1DBSdCGc9e0axvYScSaGf28')
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
